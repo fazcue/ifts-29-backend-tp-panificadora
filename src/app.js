38b8db3bf5f22daa9250a23eda6d9316d7f92c7e
Facundo Azcue <fazcue@gmail.com>
2026-05-09 18:42:43 -0300

    Add routes + 404 + dotenv

--- src/app.js
+++ src/app.js
@@ -1,23 +1,41 @@
 import express from 'express'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
+import dotenv from 'dotenv'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+// Rutas API
+import actoresRouter from './routes/api/actoresRoutes.js'
+// Rutas Web
+import actoresRouterWeb from './routes/web/actoresRoutes.js'
 
-const PUERTO = 3000
+dotenv.config()
+
+const PUERTO = process.env.PUERTO || 3000
 const app = express()
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // Middlewares
-app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.use(express.static("public"))
 
-// Vistas Pug
+// Motor plantillas Pug
 app.set("view engine", "pug")
-app.set("views", path.join(__dirname, "views"))
+app.set("views", join(__dirname, "views"))
 
+// Rutas web
+app.use('/actores', actoresRouterWeb)
+
+// Rutas API
+app.use('/api/actores', actoresRouter)
+
+// Inicio
 app.get('/', (req, res) => res.render('portada'))
+
+// 404
+app.use('/api', (req, res) => res.status(404).json({ error: 'Recurso no encontrado' }))
+app.use((req, res) => res.status(404).render('error', { mensaje: 'Página no encontrada' }))
 
 // Servidor
 app.listen(PUERTO, () => {
-    console.log("Servidor corriendo en puerto ", PUERTO)
+    console.log(`Servidor corriendo en http://localhost:${PUERTO}`)
 })
